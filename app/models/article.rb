17862386a149f7b28aa3a5197265c39b2cc5592e
emma.morrison <emma.morrison@iinet.net.au>
2018-05-30 06:59:35 -0400

    create new template, action, and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 150 }
+  validates :title, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 50 }
   validates :blog_article, presence: true, length: { minimum: 15, maximum: 1000}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   root 'pages#home'
   
   get '/about' => 'pages#about', as: 'about'
+  
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
