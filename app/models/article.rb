4d98842387073b1fb7a86c8efa641a60251ba599
emma.morrison <emma.morrison@iinet.net.au>
2018-06-02 04:45:16 -0400

    Added more styling and refined layout.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :blog_article, presence: true, length: { minimum: 15, maximum: 1000}
+  validates :blog_article, presence: true, length: { minimum: 15 }
 end
